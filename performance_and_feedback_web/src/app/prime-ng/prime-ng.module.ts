8861fec20da31d94ad37b5ea12a7346780a78d18
meta-vaibhav-gautam <vaibhav.gautam@metacube.com>
2018-11-14 13:56:24 +0530

    primeNG module is created separately

--- performance_and_feedback_web/src/app/prime-ng/prime-ng.module.ts
+++ performance_and_feedback_web/src/app/prime-ng/prime-ng.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MenuModule} from 'primeng/menu';
 import {TabViewModule} from 'primeng/tabview';
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {CheckboxModule} from 'primeng/checkbox';
 
 @NgModule({
   exports: [
     CommonModule,
     MenuModule,
-    TabViewModule
+    TabViewModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    CheckboxModule
   ],
   declarations: []
 })
