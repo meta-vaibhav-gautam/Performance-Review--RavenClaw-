49af6de2d2fc4bf7372e12b531cb61a16f5caece
metamohitsharma <mohit.sharma@metacube.com>
2018-11-15 13:18:56 +0530

    Organisational Component Added

--- performance_and_feedback_web/src/app/organisational/organisational.component.ts
+++ performance_and_feedback_web/src/app/organisational/organisational.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RatingService} from '../rating.service';
+import {Rating} from '../rating';
 @Component({
   selector: 'app-organisational',
   templateUrl: './organisational.component.html',
   styleUrls: ['./organisational.component.css']
 })
 export class OrganisationalComponent implements OnInit {
-
-  constructor() { }
+  ratingList: Rating[];
+  constructor(private ratingService: RatingService) { }
 
   ngOnInit() {
+    this.getRatings();
   }
 
+  getRatings(): void{
+    this.ratingService.getRating().subscribe(ratings => this.ratingList = ratings);
+  }
 }
